b05edb19146a4721d24677d3ce05a56cd39a79d3
Jackie Ye <jackieye27@gmail.com>
2021-06-15 22:17:41 -0700

    implement product screen page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import products from "../products.js";
-import {Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
