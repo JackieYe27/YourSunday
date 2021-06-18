ca86b27fa72bb4879b2b1b23b2a4bcd0d6c8cd25
Jackie Ye <jackieye27@gmail.com>
2021-06-17 19:34:31 -0700

    create loading and error page

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,7 +1,7 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } from "../constants/productConstants";
 import axios from "axios";
 
-
+// exported to homeScreen to use in useEffect to make api call
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 import { listProducts } from "../actions/productAction";
 
 const HomeScreen = () => {
@@ -33,7 +35,7 @@ const HomeScreen = () => {
   return (
     <div>
       <h1>Latest Products</h1>
-      {loading ? <h1>Loading...</h1> : error ? <h3>{error}</h3> : 
+      {loading ? <Loader /> : error ? <Message variant="danger" >{error}</Message> : 
         <Row>
         {
           products.map(product => {
