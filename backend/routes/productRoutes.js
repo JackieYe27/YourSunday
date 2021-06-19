df97013eb8928da50a86c186b4aab19186378531
Jackie Ye <jackieye27@gmail.com>
2021-06-18 18:11:50 -0700

    save product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,7 @@ import { getProducts, getProductById } from "../controllers/productController.js
 
 const router = express.Router();
 
-// Fetch all products from db
-// route: GET api/products
 router.route("/").get(getProducts);
-
-// Fetch a product from db
-// route: GET api/product/:id
 router.route("/:id").get(getProductById)
 
 export default router;
