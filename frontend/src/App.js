02a0d3011eaf2a4eb41fc24d4dfa72bb307d5ca4
Jackie Ye <jackieye27@gmail.com>
2021-06-21 21:30:22 -0700

    fix capitalization error in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/cartScreen";
+import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 
