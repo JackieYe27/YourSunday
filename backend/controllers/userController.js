1c96d7e39bbd96aac7f2ec883382edb5d42bc755
Jackie Ye <jackieye27@gmail.com>
2021-06-24 20:51:18 -0700

    add admin delete functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -110,4 +110,19 @@ const getUsers = asyncHandler(async(req,res) => {
   res.json(users);
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
+// delete user
+// DELETE /api/users/:id
+// private/Admin
+const deleteUser = asyncHandler(async(req,res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove();
+    res.json({ message: "User removed" })
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser }
