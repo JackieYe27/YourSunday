7f3e05c6f97c0320d58e5303af54c8e585b81563
Jackie Ye <jackieye27@gmail.com>
2021-06-28 12:38:33 -0700

    implement upload photo file in backend

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -5,8 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
-import { listProductDetails } from "../actions/productAction";
-import { USER_UPDATE_RESET } from "../constants/userConstants";
+import { listProductDetails, updateProduct } from "../actions/productAction";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
 
 
 const ProductEditScreen = ({ match, history }) => {
@@ -25,8 +25,15 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector(state => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  const productUpdate = useSelector(state => state.productUpdate);
+  const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate;
+
 
   useEffect(() => {
+    if(successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      history.push("/admin/productlist");
+    } else {
       if (!product.name || product._id !== productId) {
         dispatch(listProductDetails(productId));
       } else {
@@ -38,11 +45,21 @@ const ProductEditScreen = ({ match, history }) => {
         setCountInStock(product.countInStock);
         setDescription(product.description);
       }
-  }, [product, dispatch, productId, history]);
+    }
+  }, [product, dispatch, productId, history, successUpdate]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    // UPDATE PRODUCT
+    dispatch(updateProduct({
+      _id: productId,
+      name,
+      price,
+      image,
+      brand,
+      category,
+      description,
+      countInStock
+    }))
   }
 
   return (
@@ -51,6 +68,8 @@ const ProductEditScreen = ({ match, history }) => {
 
     <FormContainer>
       <h1>Edit Product</h1>
+      {loadingUpdate && <Loader />}
+      {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
       {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
         <Form onSubmit={submitHandler}>
         <Form.Group controlId="name">
@@ -74,7 +93,7 @@ const ProductEditScreen = ({ match, history }) => {
         <Form.Group controlId="image">
         <Form.Label className="form-label mt-2">Image:</Form.Label>
           <Form.Control 
-            type="image" 
+            type="text" 
             placeholder="Enter image url" 
             value ={image} 
             onChange={(e) => setImage(e.target.value)}>
