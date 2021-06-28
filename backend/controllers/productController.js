4c96f2a49e9c0947855ce7fe80b7b0d45f379314
Jackie Ye <jackieye27@gmail.com>
2021-06-28 11:38:38 -0700

    implement backend for update and create new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -37,4 +37,49 @@ const deleteProduct = asyncHandler(async(req,res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct } 
+// Create a product
+// route: POST /api/products
+const createProduct = asyncHandler(async(req,res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image:"/images/sample.jpg",
+    brand: "sample brand",
+    category:"Samle category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description"
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// Update a product
+// route: PUT /api/products/:id
+const updateProduct = asyncHandler(async(req,res) => {
+  const {name, price, description, image, brand, category, countInStock} = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.brand = brand;
+    product.image = image;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found.")
+  }
+});
+
+
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct } 
