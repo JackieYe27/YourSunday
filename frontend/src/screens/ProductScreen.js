2059ac609de21985545105213ad5acc811143020
Jackie Ye <jackieye27@gmail.com>
2021-06-29 21:29:58 -0700

    implement product rating frontend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,16 +5,25 @@ import { Form, Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 import Rating from "../components/Rating";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { listProductDetails } from "../actions/productAction";
+import { listProductDetails, createProductReview } from "../actions/productAction";
+import { PRODUCT_CREATE_REVIEW_RESET } from "../constants/productConstants";
 
 
 const ProductScreen = ({ history, match }) => {
   const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
   // const [product, setProduct] = useState({});
   const dispatch = useDispatch();
 
   const productDetails = useSelector(state => state.productDetails);
   const { loading, error, product } = productDetails;
+
+  const userLogin = useSelector(state => state.userLogin );
+  const { userInfo } = userLogin;
+
+  const productReviewCreate = useSelector(state => state.productReviewCreate);
+  const { error: errorProductReview, success: successProductReview } = productReviewCreate;
 
   // const fetchProduct = async() => {
   //   try {
@@ -27,13 +36,27 @@ const ProductScreen = ({ history, match }) => {
   // }
 
   useEffect(() => {
+    if (successProductReview) {
+      alert("Review Submitted!");
+      setRating(0);
+      setComment("");
+      dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+    }
     // fetchProduct();
     dispatch(listProductDetails(match.params.id))
-  }, [dispatch, match])
+  }, [dispatch, match, successProductReview])
 
  
   const addToCartHandler = () => {
     history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createProductReview(match.params.id, {
+      rating,
+      comment
+    }))
   }
   
   return (
@@ -111,7 +134,45 @@ const ProductScreen = ({ history, match }) => {
         </Col>
       </Row>
       }
-      
+      <Row>
+        <Col md={6}>
+          <h2>Reviews</h2>
+          {product.reviews.length === 0 && <Message>No Reviews</Message>}
+          <ListGroup variant="flush">
+             {product.reviews.map((review) => {
+              return <ListGroup.Item key={review._id}>
+                <strong>{review.name}</strong>
+                <Rating productRating={review.rating} />
+                <p>{review.createdAt.substring(0,10)}</p>
+                <p>{review.comment}</p>
+              </ListGroup.Item>
+            })}
+            <ListGroup.Item>
+              <h2>Write a Customer Review</h2>
+              {errorProductReview && <Message variant="danger">{errorProductReview}</Message>}
+              {userInfo ? (<Form onSubmit={submitHandler}>
+                <Form.Group controlId="rating">
+                  <Form.Label>Rating</Form.Label>
+                  <Form.Control as="select" value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <option value="">Select...</option>
+                    <option value="1">1 - Poor</option>
+                    <option value="2">2 - Fair</option>
+                    <option value="3">3 - Good</option>
+                    <option value="4">4 - Very Good</option>
+                    <option value="5">5 - Excellent</option>
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group controlId="comment">
+                  <Form.Label>Comment</Form.Label>
+                  <Form.Control as="textarea" row="3" value={comment} onChange={(e) => setComment(e.target.value)}></Form.Control>
+                </Form.Group>
+                <br></br>
+                <Button type="submit" variant="primary">Submit</Button>
+              </Form>) : <Message>Please <Link to="/login">Sign in</Link> to leave a review</Message>}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </div>
   )
 }
